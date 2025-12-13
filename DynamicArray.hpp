2744ef37e94a75a6df21222acfd46e52cbdbd9b0
MentalSmoge <mentalsmoge@gmail.com>
2025-12-13 14:46:27 +0500

    first

---
+++ DynamicArray.hpp
@@ -0,0 +1,271 @@
+#pragma once
+#include <utility>
+#include <cassert>
+#include <cstdlib>
+template<typename T>
+class DynamicArray
+{
+private:
+	void check_capacity()
+	{
+		if (capacity_ == length_) {
+			capacity_ = (capacity_ <= 0) ? default_capacity_ : capacity_ * 2 + 1;
+			T* new_buf = static_cast<T*>(malloc(capacity_ * sizeof(T)));
+			for (int i = 0; i < length_; ++i) {
+				new (new_buf + i) T(std::move(buf_[i]));
+				buf_[i].~T();
+			}
+			free(buf_);
+			buf_ = new_buf;
+		}
+	}
+	T* buf_;
+	int length_;
+	int capacity_;
+	const int default_capacity_ = 4;
+public:
+	#pragma region Iterators
+	class Iterator {
+	private:
+		T* ptr_;
+		int index_;
+		int size_;
+
+	public:
+		Iterator(T* ptr, int index, int size)
+			: ptr_(ptr), index_(index), size_(size) {
+		}
+
+		const T& get() const {
+			return ptr_[index_];
+		}
+
+		void set(const T& value) {
+			ptr_[index_] = value;
+		}
+
+		void next() {
+			if (hasNext()) {
+				++index_;
+			}
+		}
+
+		bool hasNext() const {
+			return index_ < size_ - 1;
+		}
+	};
+	class ReverseIterator {
+	private:
+		T* ptr_;
+		int index_;
+		int size_;
+
+	public:
+		ReverseIterator(T* ptr, int index, int size)
+			: ptr_(ptr), index_(index), size_(size) {
+		}
+
+		const T& get() const {
+			return ptr_[index_];
+		}
+
+		void set(const T& value) {
+			ptr_[index_] = value;
+		}
+
+		void next() {
+			if (hasNext()) {
+				--index_;
+			}
+		}
+
+		bool hasNext() const {
+			return index_ > 0;
+		}
+	};
+	#pragma endregion
+	DynamicArray():
+		capacity_{ default_capacity_ },
+		length_{ 0 }
+	{
+		buf_ = static_cast<T*>(malloc((capacity_) * sizeof(T)));
+	}
+	explicit DynamicArray(int capacity):
+		capacity_{ capacity },
+		length_{ 0 }
+	{
+		if (capacity_ <= 0) capacity_ = default_capacity_;
+		buf_ = static_cast<T*>(malloc((capacity_) * sizeof(T)));
+	}
+	~DynamicArray()
+	{
+		if (buf_) {
+			clear();
+			free(buf_);
+		}
+	}
+
+	DynamicArray(const DynamicArray& other)
+		: capacity_(other.capacity_), length_(other.length_) {
+		buf_ = static_cast<T*>(malloc(capacity_ * sizeof(T)));
+
+		for (int i = 0; i < length_; ++i) {
+			new (buf_ + i) T(other.buf_[i]);
+		}
+	}
+
+	DynamicArray(DynamicArray&& other) noexcept
+		: buf_(other.buf_), length_(other.length_), capacity_(other.capacity_) {
+		other.buf_ = nullptr;
+		other.length_ = 0;
+		other.capacity_ = 0;
+	}
+
+	DynamicArray& operator=(const DynamicArray& other) {
+		if (this != &other) {
+			clear();
+			free(buf_);
+
+			capacity_ = other.capacity_;
+			length_ = other.length_;
+			buf_ = static_cast<T*>(malloc(capacity_ * sizeof(T)));
+
+			for (int i = 0; i < length_; ++i) {
+				new (buf_ + i) T(other.buf_[i]);
+			}
+		}
+		return *this;
+	}
+
+	DynamicArray& operator=(DynamicArray&& other) noexcept {
+		if (this != &other) {
+			clear();
+			free(buf_);
+
+			buf_ = other.buf_;
+			length_ = other.length_;
+			capacity_ = other.capacity_;
+
+			other.buf_ = nullptr;
+			other.length_ = 0;
+			other.capacity_ = 0;
+		}
+		return *this;
+	}
+
+	void swap(DynamicArray& other) noexcept {
+		std::swap(buf_, other.buf_);
+		std::swap(length_, other.length_);
+		std::swap(capacity_, other.capacity_);
+	}
+
+	int insert(const T& value)
+	{
+		check_capacity();
+		new (buf_ + length_) T(value);
+		length_ += 1;
+		return length_ - 1;
+	}
+	int insert(int index, const T& value) {
+		assert(index >= 0 && index <= length_);
+		check_capacity();
+		for (int i = length_; i > index; --i) {
+			new (buf_ + i) T(buf_[i - 1]);
+			buf_[i - 1].~T();
+		}
+
+		new (buf_ + index) T(value);
+		length_ += 1;
+
+		return index;
+	}
+
+	int insert(T&& value) {
+		check_capacity();
+		new (buf_ + length_) T(std::move(value));
+		length_ += 1;
+		return length_ - 1;
+	}
+
+	int insert(int index, T&& value) {
+		assert(index >= 0 && index <= length_);
+
+		check_capacity();
+
+		for (int i = length_; i > index; --i) {
+			new (buf_ + i) T(std::move(buf_[i - 1]));
+			buf_[i - 1].~T();
+		}
+
+		new (buf_ + index) T(std::move(value));
+		length_ += 1;
+
+		return index;
+	}
+
+	void remove(int index) {
+		assert(index >= 0 && index < length_);
+
+		buf_[index].~T();
+
+		if (std::is_move_constructible_v<T>) {
+			for (int i = index; i < length_ - 1; ++i) {
+				new (buf_ + i) T(std::move(buf_[i + 1]));
+				buf_[i + 1].~T();
+			}
+		}
+		else {
+			for (int i = index; i < length_ - 1; ++i) {
+				new (buf_ + i) T(buf_[i + 1]);
+				buf_[i + 1].~T();
+			}
+		}
+
+		length_ -= 1;
+	}
+
+	T& operator[](int index) {
+		assert(index >= 0 && index < length_);
+		return buf_[index];
+	}
+
+	const T& operator[](int index) const {
+		assert(index >= 0 && index < length_);
+		return buf_[index];
+	}
+
+	int size() const {
+		return length_;
+	}
+
+	void clear() {
+		for (int i = 0; i < length_; ++i) {
+			buf_[i].~T();
+		}
+		length_ = 0;
+	}
+
+	T* begin() {
+		return buf_;
+	}
+
+	T* end() {
+		return buf_ + length_;
+	}
+
+	Iterator iterator() {
+		return Iterator(buf_, 0, length_);
+	}
+
+	Iterator iterator() const {
+		return Iterator(const_cast<T*>(buf_), 0, length_);
+	}
+
+	ReverseIterator reverseIterator() {
+		return ReverseIterator(buf_, length_ - 1, length_);
+	}
+
+	ReverseIterator reverseIterator() const {
+		return ReverseIterator(const_cast<T*>(buf_), length_ - 1, length_);
+	}
+};
---
+++ DynamicArray_Tests.hpp
@@ -0,0 +1,305 @@
+#pragma once
+#include <gtest/gtest.h>
+#include <string>
+#include "DynamicArray.hpp"
+
+#pragma region Constructors
+TEST(DynamicArrayTest, DefaultConstructor) {
+    DynamicArray<int> arr;
+    EXPECT_EQ(arr.size(), 0);
+}
+TEST(DynamicArrayTest, ConstructorWithCapacity) {
+    DynamicArray<int> arr(16);
+    EXPECT_EQ(arr.size(), 0);
+}
+
+TEST(DynamicArrayTest, CopyConstructor) {
+    DynamicArray<int> arr1;
+
+    for (int i = 0; i < 10; ++i) {
+        arr1.insert(i * 10);
+    }
+
+    DynamicArray<int> arr2 = arr1;
+
+    EXPECT_EQ(arr2.size(), arr1.size());
+
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_EQ(arr2[i], arr1[i]);
+    }
+
+    arr1[0] = 999;
+    EXPECT_EQ(arr2[0], 0);
+}
+
+TEST(DynamicArrayTest, MoveConstructor) {
+    DynamicArray<int> arr1;
+
+    for (int i = 0; i < 10; ++i) {
+        arr1.insert(i * 10);
+    }
+
+    int old_size = arr1.size();
+
+    DynamicArray<int> arr2 = std::move(arr1);
+
+    EXPECT_EQ(arr2.size(), old_size);
+    EXPECT_EQ(arr1.size(), 0);
+
+    for (int i = 0; i < 10; ++i) {
+        EXPECT_EQ(arr2[i], i * 10);
+    }
+}
+
+TEST(DynamicArrayTest, CopyAssignment) {
+    DynamicArray<int> arr1;
+
+    for (int i = 0; i < 5; ++i) {
+        arr1.insert(i * 10);
+    }
+
+    DynamicArray<int> arr2 = arr1;
+
+    EXPECT_EQ(arr2.size(), arr1.size());
+
+    for (int i = 0; i < 5; ++i) {
+        EXPECT_EQ(arr2[i], arr1[i]);
+    }
+}
+
+TEST(DynamicArrayTest, MoveAssignment) {
+    DynamicArray<int> arr1;
+
+    for (int i = 0; i < 5; ++i) {
+        arr1.insert(i * 10);
+    }
+
+    int old_size = arr1.size();
+
+    DynamicArray<int> arr2 = std::move(arr1);
+
+    EXPECT_EQ(arr2.size(), old_size);
+    EXPECT_EQ(arr1.size(), 0);
+}
+
+TEST(DynamicArrayTest, Swap) {
+    DynamicArray<int> arr1;
+    arr1.insert(1);
+    DynamicArray<int> arr2;
+    arr2.insert(2);
+    arr2.insert(3);
+
+    arr1.swap(arr2);
+    EXPECT_EQ(arr1[0], 2);
+    EXPECT_EQ(arr1[1], 3);
+
+    EXPECT_EQ(arr2[0], 1);
+
+    EXPECT_EQ(arr1.size(), 2);
+    EXPECT_EQ(arr2.size(), 1);
+}
+
+#pragma endregion
+#pragma region Iterators
+TEST(DynamicArrayTest, ForwardIterator) {
+    DynamicArray<int> arr;
+
+    for (int i = 0; i < 5; ++i) {
+        arr.insert(i * 10);
+    }
+
+    auto it = arr.iterator();
+    int expected = 0;
+
+    while (true) {
+        EXPECT_EQ(it.get(), expected);
+        expected += 10;
+
+        if (!it.hasNext()) break;
+        it.next();
+    }
+
+    auto it2 = arr.iterator();
+    it2.set(999);
+    EXPECT_EQ(arr[0], 999);
+}
+
+TEST(DynamicArrayTest, ReverseIterator) {
+    DynamicArray<int> arr;
+
+    for (int i = 0; i < 5; ++i) {
+        arr.insert(i * 10);
+    }
+
+    auto it = arr.reverseIterator();
+    int expected = 40;
+
+    while (true) {
+        EXPECT_EQ(it.get(), expected);
+        expected -= 10;
+
+        if (!it.hasNext()) break;
+        it.next();
+    }
+}
+TEST(DynamicArrayTest, ForwardIteratorString) {
+    DynamicArray<std::string> arr;
+
+    arr.insert("iterator");
+    arr.insert("test");
+    arr.insert("for");
+    arr.insert("strings");
+
+    auto it = arr.iterator();
+
+    EXPECT_EQ(it.get(), "iterator");
+
+    it.next();
+    EXPECT_EQ(it.get(), "test");
+
+    it.next();
+    EXPECT_EQ(it.get(), "for");
+
+    it.next();
+    EXPECT_EQ(it.get(), "strings");
+
+    EXPECT_FALSE(it.hasNext());
+
+    auto it2 = arr.iterator();
+    it2.set("modified");
+    EXPECT_EQ(arr[0], "modified");
+
+    auto it3 = arr.iterator();
+    int count = 0;
+    while (true) {
+        ++count;
+        if (!it3.hasNext()) break;
+        it3.next();
+    }
+    EXPECT_EQ(count, 4);
+}
+#pragma endregion
+#pragma region BasicOperations
+TEST(DynamicArrayTest, InsertAndAccess) {
+    DynamicArray<int> arr;
+    int x = 10, y = 20, z = 30;
+    arr.insert(x);
+    arr.insert(std::move(y));
+    arr.insert(30);
+
+    EXPECT_EQ(arr.size(), 3);
+    EXPECT_EQ(arr[0], 10);
+    EXPECT_EQ(arr[1], 20);
+    EXPECT_EQ(arr[2], 30);
+}
+TEST(DynamicArrayTest, InsertAtIndex) {
+    DynamicArray<int> arr;
+
+    arr.insert(10);    // [10]
+    arr.insert(30);    // [10, 30]
+    arr.insert(1, 20); // [10, 20, 30]
+
+    EXPECT_EQ(arr.size(), 3);
+    EXPECT_EQ(arr[0], 10);
+    EXPECT_EQ(arr[1], 20);
+    EXPECT_EQ(arr[2], 30);
+
+    arr.insert(0, 5); // [5, 10, 20, 30]
+    EXPECT_EQ(arr[0], 5);
+    EXPECT_EQ(arr.size(), 4);
+}
+TEST(DynamicArrayTest, RemoveElement) {
+    DynamicArray<int> arr;
+
+    for (int i = 0; i < 5; ++i) {
+        arr.insert(i * 10);
+    }
+    // [0, 10, 20, 30, 40]
+
+    arr.remove(2);
+    // [0, 10, 30, 40]
+
+    EXPECT_EQ(arr.size(), 4);
+    EXPECT_EQ(arr[0], 0);
+    EXPECT_EQ(arr[1], 10);
+    EXPECT_EQ(arr[2], 30);
+    EXPECT_EQ(arr[3], 40);
+
+    arr.remove(0); // [10, 30, 40]
+    EXPECT_EQ(arr.size(), 3);
+    EXPECT_EQ(arr[0], 10);
+
+    arr.remove(arr.size() - 1); // [10, 30]
+    EXPECT_EQ(arr.size(), 2);
+    EXPECT_EQ(arr[0], 10);
+    EXPECT_EQ(arr[1], 30);
+}
+TEST(DynamicArrayTest, ModifyElements) {
+    DynamicArray<int> arr;
+
+    arr.insert(1);
+    arr.insert(2);
+    arr.insert(3);
+
+    arr[0] = 100;
+    arr[1] = 200;
+    arr[2] = 300;
+
+    EXPECT_EQ(arr[0], 100);
+    EXPECT_EQ(arr[1], 200);
+    EXPECT_EQ(arr[2], 300);
+}
+
+
+
+TEST(DynamicArrayTest, ClearArray) {
+    DynamicArray<int> arr;
+
+    for (int i = 0; i < 10; ++i) {
+        arr.insert(i);
+    }
+
+    EXPECT_EQ(arr.size(), 10);
+
+    arr.clear();
+    EXPECT_EQ(arr.size(), 0);
+    
+    arr.insert(100);
+    EXPECT_EQ(arr.size(), 1);
+    EXPECT_EQ(arr[0], 100);
+}
+TEST(DynamicArrayTest, InsertAndAccessString) {
+    DynamicArray<std::string> arr;
+    std::string hello = "Hello";
+    std::string test = "Test";
+    arr.insert(hello);
+    arr.insert("World");
+    arr.insert(std::move(test));
+    EXPECT_NE(test, "Test");
+
+    EXPECT_EQ(arr.size(), 3);
+    EXPECT_EQ(arr[0], "Hello");
+    EXPECT_EQ(arr[1], "World");
+    EXPECT_EQ(arr[2], "Test");
+}
+
+TEST(DynamicArrayTest, ModifyElementsString) {
+    DynamicArray<std::string> arr;
+
+    arr.insert("cat");
+    arr.insert("dog");
+    arr.insert("bird");
+
+    arr[0] = "lion";
+    arr[1] = "tiger";
+    arr[2] = "bear";
+
+    EXPECT_EQ(arr[0], "lion");
+    EXPECT_EQ(arr[1], "tiger");
+    EXPECT_EQ(arr[2], "bear");
+
+    // Модификация с использованием оператора +=
+    arr[0] += " king";
+    EXPECT_EQ(arr[0], "lion king");
+}
+#pragma endregion
